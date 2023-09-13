3b9f00522d3e62022727170976341a375cac1b1a
Luiz Humberto <luizhumberto@gmail.com>
2023-09-13 15:24:34 -0300

    chore: install and configure lint-staged #3

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+//test lint-staged
 import AppBar from './components/AppBar.vue'
 </script>
 <template>
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+//test lint-staged
+
 const app = createApp(App)
 
 registerPlugins(app)
