dfb7d023f6dde986102a435e29869a252f01432e
Luiz Humberto <luizhumberto@gmail.com>
2023-09-14 16:22:14 -0300

    feat: add state, getter and action to test #12

--- src/modules/main/store/useMainStore.ts
+++ src/modules/main/store/useMainStore.ts
@@ -1,9 +1,20 @@
 // Utilities
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useMainStore = defineStore('main', () => {
-  const mainTitle = ref('Home')
-
-  return { mainTitle }
+  const counter = ref<number>(0)
+  const increase = () => {
+    counter.value++
+  }
+  const decrease = () => {
+    counter.value--
+  }
+  const $reset = () => {
+    counter.value = 0
+  }
+  const isDisabled = computed(() => {
+    return counter.value < 1 ? true : false
+  })
+  return { counter, isDisabled, $reset, increase, decrease }
 })
--- src/modules/main/views/HomeView.vue
+++ src/modules/main/views/HomeView.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import { useMainStore } from '../store/useMainStore'
+
 const store = useMainStore()
+const { counter } = storeToRefs(store)
+const { increase, decrease } = store
+const handleUp = () => {
+  increase()
+}
+const handleDown = () => {
+  decrease()
+}
+const handleReset = () => {
+  store.$reset()
+}
 </script>
 <template>
-  <h1 class="text-center">{{ store.mainTitle }}</h1>
+  <v-container class="d-flex justify-center fill-height">
+    <v-responsive
+      class="text-center"
+      max-width="250"
+    >
+      <h1 class="text-center text-h1">{{ counter }}</h1>
+      <div class="d-flex justify-space-around pa-4">
+        <v-btn
+          color="red"
+          :disabled="store.isDisabled"
+          @click="handleDown"
+          >-</v-btn
+        >
+        <v-btn
+          color="green"
+          @click="handleUp"
+          >+</v-btn
+        >
+      </div>
+      <v-btn
+        block
+        color="red"
+        @click="handleReset"
+        >Reset</v-btn
+      >
+    </v-responsive>
+  </v-container>
 </template>
