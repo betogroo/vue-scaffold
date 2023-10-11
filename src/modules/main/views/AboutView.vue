6d9f46ca358d88acf5ee4eb2972fe69430e485d9
betogroo <luizhumberto@gmail.com>
2023-10-11 17:24:54 -0300

    chore: add vue and vuetify version to about page #23

--- src/modules/main/views/AboutView.vue
+++ src/modules/main/views/AboutView.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
+import { version as vuetifyVersion } from 'vuetify'
+import { version as vueVersion } from 'vue'
 import { useMainStore } from '../store/useMainStore'
 const store = useMainStore()
 </script>
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <v-img
-        height="300"
-        src="@/assets/logo.svg"
-      />
-      <h5 class="text-h2 font-weight-bold">Vuetify</h5>
-      <h5 class="text-h6">The HomeView counter value is {{ store.counter }}</h5>
+      <v-row>
+        <v-col>
+          <v-img src="@/assets/vue.svg" />
+          {{ vueVersion }}
+        </v-col>
+        <v-col>
+          <v-img src="@/assets/logo.png" />
+          {{ vuetifyVersion }}
+        </v-col>
+
+        <v-col cols="12">
+          <div class="text-body-1">Testing Store</div>
+
+          <h5 class="text-h6">
+            The HomeView counter value is {{ store.counter }}
+          </h5>
+        </v-col>
+      </v-row>
+
       <div class="py-14" />
     </v-responsive>
   </v-container>
