a786e7152e9a1b98d5af2dbb8ae26abcf5d62f34
betogroo <luizhumberto@gmail.com>
2024-01-04 12:27:03 -0300

    feat: add app version and name

--- src/modules/main/components/AppVersion.vue
+++ src/modules/main/components/AppVersion.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { version as vuetifyVersion } from 'vuetify'
 import { version as vueVersion } from 'vue'
+const appVersion = __APP_VERSION__
+const appName = __APP_NAME__
 </script>
 
 <template>
   <v-row>
+    <v-col cols="12">
+      <h5>{{ appName }}</h5>
+      {{ appVersion }}
+    </v-col>
     <v-col>
       <v-img src="@/assets/vue.svg" />
       {{ vueVersion }}
