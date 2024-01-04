a8f2aad4d7f2e8a2f151d7728cc01e3f679b2f83
betogroo <luizhumberto@gmail.com>
2024-01-04 11:24:04 -0300

    refactor: create a separate component to app version

---
+++ src/modules/main/components/AppVersion.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { version as vuetifyVersion } from 'vuetify'
+import { version as vueVersion } from 'vue'
+</script>
+
+<template>
+  <v-row>
+    <v-col>
+      <v-img src="@/assets/vue.svg" />
+      {{ vueVersion }}
+    </v-col>
+    <v-col>
+      <v-img src="@/assets/logo.png" />
+      {{ vuetifyVersion }}
+    </v-col>
+  </v-row>
+</template>
