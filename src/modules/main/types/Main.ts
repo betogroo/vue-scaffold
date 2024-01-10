0a9a3c823ad602ff653300ed3bff08e686c76094
betogroo <luizhumberto@gmail.com>
2024-01-10 09:12:24 -0300

    feat: add form data view

--- src/modules/main/types/Main.ts
+++ src/modules/main/types/Main.ts
@@ -3,11 +3,7 @@ import { toTypedSchema } from '@vee-validate/zod'
 
 export const UserSchema = z.object({
   full_name: z.string().min(1, 'O nome é obrigatório'),
-  email: z
-    .string()
-    .email('Email Inválido')
-    .min(1, 'O email é obrigatório')
-    .default('ze@ze.com.br'),
+  email: z.string().email('Email Inválido').min(1, 'O email é obrigatório'),
   gender: z.enum(['M', 'F']),
 })
 
